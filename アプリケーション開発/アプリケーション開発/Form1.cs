cc575b365f2f2cd1de5c71fedb85cb8eaa44a797
GEIST\zinbe <zinbei.4286@gmail.com>
2025-12-05 16:08:44 +0900

    授業終了につき途中のまま

--- アプリケーション開発/アプリケーション開発/Form1.cs
+++ アプリケーション開発/アプリケーション開発/Form1.cs
@@ -12,9 +12,33 @@ namespace アプリケーション開発
 {
     public partial class Form1 : Form
     {
+        GameManeger game;
+        private Panel GamePanel;
+        private Timer GameTimer;
         public Form1()
         {
             InitializeComponent();
+
+            GamePanel = new Panel();
+            GamePanel.Location = new Point(10, 10);      // 位置
+            GamePanel.Size = new Size(400, 300);         // サイズ
+            GamePanel.BackColor = Color.Black;           // 背景色
+            this.Controls.Add(GamePanel);
+            DoubleBuffered = true;
+
+            game=new GameManeger(GamePanel.Width,GamePanel.Height);
+            GameTimer.Interval = 16;
+            GameTimer.Start();
+        }
+        private void GameTimer_Tick(object sender, EventArgs e) 
+        {
+            game.Update();
+            GamePanel.Invalidate();
+        }
+
+        private void Game_Panel(object sender, PaintEventArgs e) 
+        {
+            game.Draw(e.graphics);
         }
     }
 }
