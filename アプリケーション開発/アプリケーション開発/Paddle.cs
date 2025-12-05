cf9f33546cd34c84fb799a5a13381aec6e61f858
GEIST\zinbe <zinbei.4286@gmail.com>
2025-12-05 14:22:02 +0900

    パドル描画

--- アプリケーション開発/アプリケーション開発/Paddle.cs
+++ アプリケーション開発/アプリケーション開発/Paddle.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace アプリケーション開発
 {
-        public class paddle 
-        {
+    public class paddle
+    {
         public float X, Y;      //初期値設定
         public int width = 100;
-        public int height =15 ;
+        public int height = 15;
 
         public paddle(float x, float y)
         {
             X = x;
             Y = y;          //ここまで
         }
+        public void MoveTo(float mouseX)
+        {
+            X = mouseX - width / 2;
+        }
 
+        public void Draw(Graphics g, Block block)
+        {
+            g.FillRectangle(Brushes.Blue, block.Rect);
+        }
     }
-    }
+}
 
